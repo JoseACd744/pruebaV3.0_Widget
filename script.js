1bbb83e2d88222cd87b58997202ff4016f6a8718
José Anthony Arenas Conde <u20211d744@upc.edu.pe>
2024-07-18 22:54:51 -0500

    feat: Add lead data calculator functionality and update button styles

--- script.js
+++ script.js
@@ -7,11 +7,12 @@ define(['jquery'], function ($) {
         return true;
       },
       init: function () {
+        self.loadCSS();
         return true;
       },
       bind_actions: function () {
-        $('#fetch-lead-data-btn').on('click', function () {
-          self.fetchLeadData();
+        $('#calculate-btn').on('click', function () {
+          self.calculate();
         });
         return true;
       },
@@ -19,11 +20,11 @@ define(['jquery'], function ($) {
         self.render_template({
           caption: {
             class_name: 'js-km-caption',
-            html: 'Lead Data Fetcher'
+            html: 'Lead Data Calculator'
           },
           body: '<div class="km-form">\
-                   <button id="fetch-lead-data-btn">Fetch Lead Data</button>\
-                   <div id="lead-data-display"></div>\
+                   <button id="calculate-btn">Calculate</button>\
+                   <div id="calculation-result"></div>\
                  </div>',
           render: ''
         });
@@ -40,12 +41,52 @@ define(['jquery'], function ($) {
       destroy: function () {}
     };
 
-    this.fetchLeadData = function () {
+    this.loadCSS = function() {
+      var settings = self.get_settings();
+      if ($('link[href="' + settings.path + '/style.css?v=' + settings.version + '"').length < 1) {
+        $('head').append('<link href="' + settings.path + '/style.css?v=' + settings.version + '" type="text/css" rel="stylesheet">');
+      }
+    };
+
+    this.calculate = function () {
       var leadData = APP.data.current_card;
-      var displayDiv = $('#lead-data-display');
-      displayDiv.empty();
-      displayDiv.append('<p>Lead ID: ' + leadData.id + '</p>');
-      displayDiv.append('<p>Attributes: ' + JSON.stringify(leadData.attributes) + '</p>');
+      var attributes = leadData.fields_hider.model.attributes;
+
+      if (leadData && attributes) {
+        var meses = parseInt(attributes['CFV[2959884]'] || '0', 10);
+        var usuarios = parseInt(attributes['CFV[2959886]'] || '0', 10);
+        var planKommo = attributes['CFV[2959888]'] || '';
+
+        console.log('Meses:', meses);
+        console.log('Usuarios:', usuarios);
+        console.log('Plan Kommo:', planKommo);
+
+        var planValue;
+        switch (planKommo) {
+          case "9227454": // Básico
+            planValue = 10;
+            break;
+          case "9227456": // Avanzado
+            planValue = 15;
+            break;
+          case "9227458": // Empresarial
+            planValue = 30;
+            break;
+          default:
+            console.log('Unexpected planKommo value:', planKommo);
+            planValue = 0;
+        }
+
+        console.log('Plan Value:', planValue);
+
+        var result = meses * usuarios * planValue;
+
+        var displayDiv = $('#calculation-result');
+        displayDiv.empty();
+        displayDiv.append('<p>Calculation Result: ' + result + '</p>');
+      } else {
+        console.error('Lead data or attributes are undefined');
+      }
     };
 
     return this;
